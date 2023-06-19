b7cd827921484d69560c541159d218d78f14123f
EduardoRoesler <eduardo.roesler@gmail.com>
2023-06-19 18:58:32 -0300

    19/06/23_001

--- aulas-react/src/App.js
+++ aulas-react/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+  const name ="Eduardo"
+  const newName = name.toUpperCase()
+  const url = "https://via.placeholder.com/150"
+
+  function soma(a,b){
+    return a+b
+  }
+          //Observar que no react se utiliza className=""
   return (
-    <div className="App">
-      <h1> Olá, mundo! </h1>
-        <p> Meu primeiro app </p>
+    <div className="App"> 
+      <h1> Olá, {newName} </h1>
+      <p>Soma: 2+2 = {2+2}</p>
+      <p>Soma com função: 3+3 = {soma(3,3)}</p>
+      <img src={url} alt="Imagem"></img>
     </div>
   );
 }
